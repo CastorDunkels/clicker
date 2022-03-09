b4004a050af4fd4f971ae6fa4770a15d81e01ed9
Castor Dunkels <castordnk@gmail.com>
2022-03-09 22:04:34 +0100

    fixade med hp och lite med nugget

--- app.js
+++ app.js
@@ -3,23 +3,50 @@ let health = 1;
 let HealthButton = document.querySelector(".NuggetHealth");
 let NuggetButton = document.querySelector(".Nugget");
 let NuggetCounter = document.querySelector(".Nugget-count");
+let HealthDisplay = document.querySelector(".HP");
 
 NuggetButton.addEventListener("click", Nugget);
 HealthButton.addEventListener("click", NuggetHealth);
 
-function Nugget() {
-
-rndNum = Math.floor(Math.random() * 10);  //gotta fix health stuff but not right now
-if (rndNum == 0) {
-    nugget += 1 *health
+function SetNugget(n) {
+    nugget = n;
+    NuggetCounter.innerHTML = "Nugget-count: " + nugget + "st";
 }
-NuggetCounter.innerHTML = "Nugget-count: " + nugget + "st";
-console.log(health);
+function Nugget(event) { 
+    console.log(event);
+    if(event.pointerType === ""){
+        return;
+    }
+    rndNum = Math.floor(Math.random() * 10); 
+    if (rndNum == 0) {
+        SetNugget(nugget + 1); 
+    }
+
+    console.log(health);
+}
+function SetHealth(h){
+    /*if(h === 0){
+
+    }*/
+    if(h < 0){
+        h = 0;
+    }
+    if(h > 10){
+        h = 10;
+    } 
+    health = h;
+    HealthDisplay.innerHTML = health;
 }
 
 function NuggetHealth() {
-    if (nugget >= 1) {
-        nugget--;
-        health ++;
+    if (nugget >= 1 && (health > 1 || health < 10)) {
+        SetNugget(nugget - 1);
+        SetHealth(health + 2);
     }
 }
+
+SetHealth(10);
+
+setInterval(() => {
+    SetHealth(health - 1);
+}, 2000);
